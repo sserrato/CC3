6cca15d0840958be467652d23469a22ac3b03915
Sergio Serrato <serrato@cal.berkeley.edu>
2015-08-02 14:35:04 -0700

    changing controller for jobs to associate skills.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,13 +5,12 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
-    @job.skills.new 
+    @job.skills.new
   end
 
   def create
     @job = Job.new job_params
-    # @skills = @job.skills.new
-    if @job.save # && @skills.save
+    if @job.save
       redirect_to jobs_path
     else
       render 'new'
